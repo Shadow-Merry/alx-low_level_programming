87504cf6293e20bbdb56a840e2fc7b66bec320fa
Shadow-Merry <merryrock6@gmail.com>
2022-04-07 04:15:51 -0700

    done five

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
 /**
- * main functaion
+ * main - functaion
+ *
+ * Return: 0 (sucess)
  */
-int main()
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return 0;
+		return (0);
 }
