be9831c1a9071fb094aa1280d003e226d0071518
Shadow-Merry <merryrock6@gmail.com>
2022-04-12 04:09:18 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - function
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int n;
+	int number;
+
+	for (number = 0; number < 10; number++)
+		for (n = 'a'; n <= 'z'; n++)
+			_putchar(n);
+		_putchar('\n');
+}
