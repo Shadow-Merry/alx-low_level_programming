fdc054f75d140d5020886e7eef347edc4805aa87
Shadow-Merry <merryrock6@gmail.com>
2022-04-12 04:47:26 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - function
+ * @int: input
+ * Return: int
+ */
+int _abs(int)
+{
+	int n;
+
+	if (n <= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (n * -1);
+	}
+}
