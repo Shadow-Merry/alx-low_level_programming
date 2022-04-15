1ecabc8bc0a92aae17d25f421b0ef72e258538d3
Shadow-Merry <merryrock6@gmail.com>
2022-04-15 02:33:27 -0700

    task 6

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_diagonal - function
+ * @n: input
+ * Return: none
+ */
+void print_diagonal(int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while (i < n)
+	{
+		while (j <= i)
+		{
+			if (j != i)
+				_putchar(' ');
+			else
+				_putchar(92);
+			j += 1;
+		}
+		_putchar('\n');
+		i += 1;
+		j = 0;
+	}
+	if (n <= 0)
+		_putchar('\n');
+}
