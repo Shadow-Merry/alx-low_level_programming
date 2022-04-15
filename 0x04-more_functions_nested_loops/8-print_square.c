6e60209d820810d92a1c9443a4d5c0cb004cb8b0
Shadow-Merry <merryrock6@gmail.com>
2022-04-15 02:37:19 -0700

    task 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_square - function
+ * @size: size
+ * Return: none
+ */
+void print_square(int size)
+{
+	int col, row;
+
+	if (size > 0)
+	{
+		for (row = 0; row < size; row++)
+		{
+			for (col = 0; col < size; col++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
