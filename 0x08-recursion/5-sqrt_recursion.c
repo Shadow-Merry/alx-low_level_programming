a0dabc7ef23848e03d0cb1c22ee9d5e88de6d012
Shadow-Merry <merryrock6@gmail.com>
2022-04-27 11:35:11 -0700

    task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,21 @@
 #include "main.h"
 
 /**
+ * sqrt_check - checks
+ * @g: guess
+ * @c: number
+ * Return: the squre
+ */
+int sqrt_check(int g, int c)
+{
+	if (g * g == c)
+		return (g);
+	if (g * g > c)
+		return (-1);
+	return (sqrt_check(g + 1, c));
+}
+
+/**
  * _sqrt_recursion - function
  * @n: number
  * Return: natural square
@@ -9,5 +24,5 @@ int _sqrt_recursion(int n)
 {
 	if (n == 0)
 		return (0);
-	return (_sqrt_recursion(1, n));
+	return (sqrt_check(1, n));
 }
