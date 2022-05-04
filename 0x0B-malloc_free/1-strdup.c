a160b973170e6acefdbcdc9a58a831359928e5c8
Shadow-Merry <merryrock6@gmail.com>
2022-05-03 23:41:12 -0700

    task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -16,17 +16,18 @@ char *_strdup(char *str)
 	if (str == NULL)
 		return (NULL);
 	for (n = 0; str[n] != '\0'; n++)
-		;
-	strnew = (char *)mallox(n + 1 * sizeof(char));
-	if (strnew != NULL)
 	{
-		for (i = 0; str[i] != '\0'; i++)
-			strnew[i] = str[i];
+		strnew = (char *)mallox(n + 1 * sizeof(char));
+		if (strnew != NULL)
+		{
+			for (i = 0; str[i] != '\0'; i++)
+				strnew[i] = str[i];
+		}
+		else
+		{
+			return (NULL);
+		}
+		strnew[i] = '\0';
 	}
-	else
-	{
-		return (NULL);
-	}
-	strnew[i] = '\0';
 	return (strnew);
 }
