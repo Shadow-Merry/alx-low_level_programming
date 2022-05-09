fea83c6777a4ad63ab5743a1574618a00cd02957
Shadow-Merry <merryrock6@gmail.com>
2022-05-09 14:10:40 -0700

    task 0 - 2

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,27 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - definition
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * Description: dog 
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dog_t - typedef for dog
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+
+#endif
