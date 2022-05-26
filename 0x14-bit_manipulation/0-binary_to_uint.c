ebe5e425103735141f945528c666223129f40917
Shadow-Merry <merryrock6@gmail.com>
2022-05-26 12:44:55 -0700

    task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,13 +12,11 @@ unsigned int binary_to_uint(const char *b)
 
 	if (!b)
 		return (0);
-	else
-		for (i = 0; b[i]; i++)
-		{
-			if (b[i] > '1' || b[i] < '0')
-				return (0);
-			val = 2 * val + (b[i] - '0');
-		}
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] > '1' || b[i] < '0')
+			return (0);
+		val = 2 * val + (b[i] - '0');
+	}
 	return (val);
-}
-		
+}	
